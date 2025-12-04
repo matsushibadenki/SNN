349bf776695253f111e9a3abcbbbfe1ab07e8fd8
matsushibadenki <littlebuddha@mac.com>
2025-12-04 17:18:28 +0900

    Update __init__.py

--- snn_research/bio_models/__init__.py
+++ snn_research/bio_models/__init__.py
@@ -1,7 +1,10 @@
 # ファイルパス: snn_research/bio_models/__init__.py
-# (新規作成)
+# Title: 生物学的モデルパッケージ初期化 (修正版)
+# Description:
+#   snn_research.models.bio パッケージへのエイリアスとして機能します。
+#   mypyエラー [import-not-found] を修正。
 
-from .lif_neuron_legacy import BioLIFNeuron
-from .simple_network import BioSNN
+from snn_research.models.bio.lif_neuron_legacy import BioLIFNeuron
+from snn_research.models.bio.simple_network import BioSNN
 
 __all__ = ["BioLIFNeuron", "BioSNN"]
