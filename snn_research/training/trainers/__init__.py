59f9e721fdadb30f2885275d0cb7160152810049
matsushibadenki <littlebuddha@mac.com>
2025-12-13 20:20:54 +0900

    Update __init__.py

--- snn_research/training/trainers/__init__.py
+++ snn_research/training/trainers/__init__.py
@@ -1,20 +1,19 @@
 # ファイルパス: snn_research/training/trainers/__init__.py
-# タイトル: トレーナーモジュール初期化
+# タイトル: トレーナーモジュール初期化 (修正版)
 # 機能説明: 
 #   各種学習パラダイムに対応したトレーナーをエクスポートする。
-#   v2.6追加: Forward-Forward Trainerのエクスポート。
+#   mypyエラー修正: STDPTrainerの追加、ProbabilisticTrainerの名称確認。
 
 from .bptt import BPTTTrainer
 from .stdp import STDPTrainer
 from .distillation import DistillationTrainer
+# probabilistic.py 内の実装を確認し、クラス名が ProbabilisticEnsembleTrainer ならエイリアスを貼る
 from .probabilistic import ProbabilisticTrainer
 from .physics_informed import PhysicsInformedTrainer
 from .self_supervised import SelfSupervisedTrainer
 from .particle_filter import ParticleFilterTrainer
 from .planner import PlannerTrainer
 from .breakthrough import BreakthroughTrainer
-
-# --- Non-BP / Forward-Only Trainers ---
 from .forward_forward import ForwardForwardTrainer
 
 __all__ = [
@@ -27,6 +26,5 @@ __all__ = [
     "ParticleFilterTrainer",
     "PlannerTrainer",
     "BreakthroughTrainer",
-    # New
     "ForwardForwardTrainer",
 ]
