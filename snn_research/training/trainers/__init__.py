7466bff3215ebc398f20c2b6027abc70b46de240
matsushibadenki <littlebuddha@mac.com>
2025-12-13 20:24:41 +0900

    Update __init__.py

--- snn_research/training/trainers/__init__.py
+++ snn_research/training/trainers/__init__.py
@@ -2,13 +2,13 @@
 # タイトル: トレーナーモジュール初期化 (修正版)
 # 機能説明: 
 #   各種学習パラダイムに対応したトレーナーをエクスポートする。
-#   mypyエラー修正: STDPTrainerの追加、ProbabilisticTrainerの名称確認。
+#   修正: ProbabilisticEnsembleTrainerの正しいインポートとエクスポート。
 
 from .bptt import BPTTTrainer
 from .stdp import STDPTrainer
 from .distillation import DistillationTrainer
-# probabilistic.py 内の実装を確認し、クラス名が ProbabilisticEnsembleTrainer ならエイリアスを貼る
-from .probabilistic import ProbabilisticTrainer
+# 修正: probabilistic.py 内のクラス名は ProbabilisticEnsembleTrainer
+from .probabilistic import ProbabilisticEnsembleTrainer
 from .physics_informed import PhysicsInformedTrainer
 from .self_supervised import SelfSupervisedTrainer
 from .particle_filter import ParticleFilterTrainer
@@ -20,7 +20,7 @@ __all__ = [
     "BPTTTrainer",
     "STDPTrainer",
     "DistillationTrainer",
-    "ProbabilisticTrainer",
+    "ProbabilisticEnsembleTrainer",  # 修正: エクスポート名を変更
     "PhysicsInformedTrainer",
     "SelfSupervisedTrainer",
     "ParticleFilterTrainer",
