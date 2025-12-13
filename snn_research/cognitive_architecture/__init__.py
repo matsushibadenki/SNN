934536538c324e124be162b5f493efd2b0806e5d
matsushibadenki <littlebuddha@mac.com>
2025-12-13 20:03:13 +0900

    Update __init__.py

--- snn_research/cognitive_architecture/__init__.py
+++ snn_research/cognitive_architecture/__init__.py
@@ -1,54 +1,63 @@
 # ファイルパス: snn_research/cognitive_architecture/__init__.py
-# (更新)
-# Title: 認知アーキテクチャパッケージ初期化
+# タイトル: 認知アーキテクチャ パッケージ初期化
+# 機能説明: 
+#   主要な認知モジュールをエクスポートし、外部からのアクセスを容易にする。
+#   v2.6追加: HDCエンジン、Tsetlin Machineのエクスポート。
 
-from .astrocyte_network import AstrocyteNetwork
-from .emergent_system import EmergentCognitiveSystem
+from .artificial_brain import ArtificialBrain, BrainConfig
 from .global_workspace import GlobalWorkspace
-from .hierarchical_planner import HierarchicalPlanner
-from .intrinsic_motivation import IntrinsicMotivationSystem
-from .meta_cognitive_snn import MetaCognitiveSNN
-from .physics_evaluator import PhysicsEvaluator
-from .planner_snn import PlannerSNN
-from .rag_snn import RAGSystem
-from .amygdala import Amygdala
-from .basal_ganglia import BasalGanglia
-from .cerebellum import Cerebellum
+from .perception_cortex import PerceptionCortex
 from .motor_cortex import MotorCortex
-from .hippocampus import Hippocampus
-from .cortex import Cortex
 from .prefrontal_cortex import PrefrontalCortex
-from .artificial_brain import ArtificialBrain
-from .hybrid_perception_cortex import HybridPerceptionCortex
-from .som_feature_map import SomFeatureMap
+from .hippocampus import Hippocampus, MemoryTrace
+from .basal_ganglia import BasalGanglia, ActionSelection
+from .cerebellum import Cerebellum
+from .amygdala import Amygdala, EmotionalState
+from .astrocyte_network import AstrocyteNetwork, MetabolicState
+from .neuromorphic_scheduler import NeuromorphicScheduler, BrainState
+from .rag_snn import RAGSystem
+from .symbol_grounding import SymbolGrounding
+from .planner_snn import PlannerSNN
+from .reasoning_engine import ReasoningEngine
+from .meta_cognitive_snn import MetaCognitiveSNN
+from .sleep_consolidation import SleepConsolidation
+from .intrinsic_motivation import IntrinsicMotivationSystem
 from .causal_inference_engine import CausalInferenceEngine
-# --- ▼ 追加 ▼ ---
-from .visual_perception import VisualCortex
-# --- ▲ 追加 ▲ ---
+from .emergent_system import EmergentSystem
+from .physics_evaluator import PhysicsEvaluator
+from .som_feature_map import SOMFeatureMap
+from .hybrid_perception_cortex import HybridPerceptionCortex
 
+# --- Green AI / Alternative Computing Modules ---
+from .hdc_engine import HDCEngine, HDCReasoningAgent
+from .tsetlin_machine import TsetlinMachine
 
 __all__ = [
-    "AstrocyteNetwork",
     "ArtificialBrain",
-    "EmergentCognitiveSystem",
+    "BrainConfig",
     "GlobalWorkspace",
-    "HierarchicalPlanner",
-    "IntrinsicMotivationSystem",
-    "MetaCognitiveSNN",
-    "PhysicsEvaluator",
-    "PlannerSNN",
-    "RAGSystem",
-    "Amygdala",
-    "BasalGanglia",
-    "Cerebellum",
+    "PerceptionCortex",
     "MotorCortex",
-    "Hippocampus",
-    "Cortex",
     "PrefrontalCortex",
-    "HybridPerceptionCortex",
-    "SomFeatureMap",
+    "Hippocampus", "MemoryTrace",
+    "BasalGanglia", "ActionSelection",
+    "Cerebellum",
+    "Amygdala", "EmotionalState",
+    "AstrocyteNetwork", "MetabolicState",
+    "NeuromorphicScheduler", "BrainState",
+    "RAGSystem",
+    "SymbolGrounding",
+    "PlannerSNN",
+    "ReasoningEngine",
+    "MetaCognitiveSNN",
+    "SleepConsolidation",
+    "IntrinsicMotivationSystem",
     "CausalInferenceEngine",
-    # --- ▼ 追加 ▼ ---
-    "VisualCortex"
-    # --- ▲ 追加 ▲ ---
+    "EmergentSystem",
+    "PhysicsEvaluator",
+    "SOMFeatureMap",
+    "HybridPerceptionCortex",
+    # New
+    "HDCEngine", "HDCReasoningAgent",
+    "TsetlinMachine",
 ]
