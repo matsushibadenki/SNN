cd98d99dbcdf83d66a9853838f31aea280a5b342
matsushibadenki <littlebuddha@mac.com>
2025-12-27 18:27:46 +0900

    Update __init__.py

--- snn_research/core/__init__.py
+++ snn_research/core/__init__.py
@@ -22,6 +22,7 @@ from .snn_core import SNNCore
 from .attention import SpikeDrivenSelfAttention
 # --- ▼ 追加 ▼ ---
 from .cortical_column import CorticalColumn
+from .ensemble_scal import EnsembleSCAL, AdaptiveEnsembleSCAL, BootstrapEnsembleSCAL
 # --- ▲ 追加 ▲ ---
 from .adaptive_neuron_selector import AdaptiveNeuronSelector
 from .adaptive_attention_selector import AdaptiveAttentionModule
@@ -34,6 +35,9 @@ __all__ = [
     "SpikeDrivenSelfAttention",
     # --- ▼ 追加 ▼ ---
     "CorticalColumn",
+    "EnsembleSCAL",
+    "AdaptiveEnsembleSCAL",
+    "BootstrapEnsembleSCAL",
     # --- ▲ 追加 ▲ ---
     "AdaptiveNeuronSelector",
     "AdaptiveAttentionModule"
